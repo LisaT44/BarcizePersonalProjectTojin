8d935abd7f56de27eb54c4e4352e62ab4f87be19
ElisaTojin <elisactojin@gmail.com>
2023-04-05 13:07:35 -0700

    I did my lab 3
    I made a player controller script for my player's movement

--- BarcizePersonalProjectTojin/Assets/Scripts/PlayerController.cs
+++ BarcizePersonalProjectTojin/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
+    private float zBound = 8;
+
     private Rigidbody playerRb;
-    private float zBound = 6;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +19,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    //Moves the player based on arrow key input
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
 
-        if(transform.position.z < -zBound)
+    //Prevent the player from leaving the top or bottom of the screen 
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
         {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
 
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
         }
     }
 }
